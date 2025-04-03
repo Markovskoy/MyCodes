797d757e5c48c6c52fbc79b9293598d22d529b0e
Victor <vicitiki913@gmail.com>
2025-04-03 23:40:40 +0300

    pypypy

---
+++ Codes/python/how_to_use_tk_libary.py
@@ -0,0 +1,10 @@
+﻿import tkinter as tk
+from tkinter import filedialog
+
+root = tk.Tk()
+root.withdraw()  # Скрываем окно
+
+file_path = filedialog.askopenfilename(title="Выберите файл")
+print(f"Выбран файл: {file_path}")
+
+root.destroy()  # Закрываем `tkinter`, чтобы он не висел в памяти
